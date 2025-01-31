9f0513377d206ab88216ad9b3ac477354ffbf9a3
Tortiolaware <tortiolaware@gmail.com>
2025-01-31 11:36:51 +0100

    crash fix

--- android/src/main/kotlin/com/matteogassend/bluetooth_classic/BluetoothClassicPlugin.kt
+++ android/src/main/kotlin/com/matteogassend/bluetooth_classic/BluetoothClassicPlugin.kt
@@ -101,11 +101,11 @@ class BluetoothClassicPlugin: FlutterPlugin, MethodCallHandler, PluginRegistry.R
   }
 
   override fun onDetachedFromActivity() {
-    TODO("Not yet implemented")
+    //TODO implement
   }
 
   override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) {
-    TODO("Not yet implemented")
+    //TODO implement
   }
 
   override fun onAttachedToActivity(binding: ActivityPluginBinding) {
@@ -113,7 +113,7 @@ class BluetoothClassicPlugin: FlutterPlugin, MethodCallHandler, PluginRegistry.R
   }
 
   override fun onDetachedFromActivityForConfigChanges() {
-    TODO("Not yet implemented")
+    //TODO implement
   }
 
   private val receiver = object: BroadcastReceiver() {
